b97f2d673bbab3de1fb822de62f9c8e61dddf3b2
Sercan <sercanorhangazi@gmail.com>
2017-10-17 09:57:07 +0300

    Lesson 2

---
+++ coder-swag/Model/Category.swift
@@ -0,0 +1,19 @@
+//
+//  Category.swift
+//  coder-swag
+//
+//  Created by Sercan on 17.10.2017.
+//  Copyright © 2017 Sercan Orhangazi. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    private(set) public var title: String!
+    private(set) public var imageName: String!
+    
+    init(title: String, imageName: String) {
+        self.title = title
+        self.imageName = imageName
+    }
+}
---
+++ coder-swag/Services/DataService.swift
@@ -0,0 +1,28 @@
+//
+//  DataService.swift
+//  coder-swag
+//
+//  Created by Sercan on 17.10.2017.
+//  Copyright © 2017 Sercan Orhangazi. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png"),
+    ]
+    
+    func getCategories() -> [Category]{
+        return categories
+    }
+    
+    
+    
+}
