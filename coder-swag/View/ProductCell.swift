e2197a8eede9102b3e771fb805c3f6d6c471cb7c
Sercan <sercanorhangazi@gmail.com>
2017-10-17 10:25:29 +0300

    CollectionView and Cell Design

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  productCell.swift
+//  coder-swag
+//
+//  Created by Sercan on 17.10.2017.
+//  Copyright © 2017 Sercan Orhangazi. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
